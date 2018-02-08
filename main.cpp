d3576d7e82b6eae551dc239d3d168f760277427b
Virgil Barcan <ghitza9404@gmail.com>
2018-02-08 19:20:27 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <benchmark/benchmark.h>
+
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+
+struct S
+{
+    uint8_t a1;
+    uint16_t a2;
+    uint32_t a3;
+    int64_t a4;
+    float x;
+    float y;
+    float z;
+    uint64_t val;
+};
+
+void BM_memset(benchmark::State &state) {
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+
+        // build struct
+        S s1;
+
+        state.ResumeTiming();
+
+        memset(&s1, 0, sizeof(s1));
+    }
+}
+
+void BM_braces(benchmark::State &state) {
+    for (auto _ : state)
+    {
+        state.PauseTiming();
+
+        // build struct
+        S s1;
+
+        state.ResumeTiming();
+
+        s1 = {};
+    }
+}
+
+BENCHMARK(BM_memset)->RangeMultiplier(2)->Range(8, 8<<10);
+BENCHMARK(BM_braces)->RangeMultiplier(2)->Range(8, 8<<10);
+
+BENCHMARK_MAIN();
